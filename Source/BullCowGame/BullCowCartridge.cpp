5897d7ee8b445084d7916c9453ccfb797f0b76e1
peppe19947 <79971281+peppe19947@users.noreply.github.com>
2022-05-17 16:22:35 +0200

    Bool

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,46 +4,55 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
 
-	// Welcome the player
-	PrintLine(TEXT("Welcome to Bull Cows!"));
-	PrintLine(TEXT("Guess 4 letters"));
-	PrintLine(TEXT("Press Enter to continue..."));
+    SetupGame();// Setting Up Game
 
-	InitGame();// Setting Up Game
-	// prompt player for guess
+    PrintLine(FString::Printf(TEXT("The hidden word is: %s.\nIs: %i characters long."), *HiddenWord, HiddenWord.Len()));
+
+    // Welcome the player
+    PrintLine(TEXT("Welcome to Bull Cows!"));
+    PrintLine(TEXT("Guess 4 letters"));
+    PrintLine(TEXT("Press Enter to continue..."));
+
+    
+    // prompt player for guess
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-	ClearScreen();
-	
-	//check player input
+    ClearScreen();
+    
+    //check player input
 
-	if (HiddenWord == Input)
-	{
-		PrintLine(TEXT("You have Won!"));
-	}
-	else
-	{
-		PrintLine(TEXT("You have Lost!"));
-	}
-	// Check If Isogram
-	// Check Right Number of Characters
+    if (HiddenWord == Input)
+    {
+        PrintLine(TEXT("You have Won!"));
+    }
+    else
+    {
+        if (Input.Len() != HiddenWord.Len())
+        {
+            PrintLine(TEXT("The hidden word is %i characters long. Try Again!"), HiddenWord.Len());//Magic Number
+        }
 
-	// Remove Life
+        PrintLine(TEXT("You have Lost!"));
+    }
+    // Check If Isogram
+    // Check Right Number of Characters
 
-	// Check if lives > 0
-	// If yes GuessAgain
-	// Show Lives Left
-	// If No Show GameOver and HiddenWord?
-	// Prompt To Play Again, Press Enter to Play Again?
-	// Check User Input
-	// Play Again or quit
+    // Remove Life
+
+    // Check if lives > 0
+    // If yes GuessAgain
+    // Show Lives Left
+    // If No Show GameOver and HiddenWord?
+    // Prompt To Play Again, Press Enter to Play Again?
+    // Check User Input
+    // Play Again or quit
 
 }
 
-void UBullCowCartridge::InitGame()
+void UBullCowCartridge::SetupGame()
 {
-	HiddenWord = TEXT("cake");
-	Lives = 4;
+    HiddenWord = TEXT("cake");
+    Lives = 4;
 }
